9d7f99e195309572b9e76bbca1651383148dedd1
radhitiaabduljabbar@gmail.com <radhitiaabduljabbar@gmail.com>
2024-10-30 19:59:52 +0700

    first commit

---
+++ fetch.js
@@ -0,0 +1,35 @@
+// API Lo
+const apiKey = 'AIzaSyDJynlnzRgHXxt0qLakeDOKxdQVc7waS00';
+// Ganti ini dengan ID channel yang mau lo cek infonya
+const channelId = 'UCkCa2NoZlJlfJlMKgZhtKOQ';
+
+async function getChannelInfo() {
+  const url = `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=${channelId}&key=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    
+    // Cek apakah response dari server sukses
+    if (!response.ok) {
+      throw new Error('Gagal mengambil data');
+    }
+    
+    const data = await response.json();
+    
+    // Ambil nama channel, jumlah subscriber, jumlah video, dan deskripsi dari data yang didapat
+    const channelName = data.items[0].snippet.title;
+    const subscriberCount = data.items[0].statistics.subscriberCount;
+    const videoCount = data.items[0].statistics.videoCount;
+    const channelDescription = data.items[0].snippet.description;
+    
+    console.log(`Nama Channel: ${channelName}`);
+    console.log(`Jumlah Subscriber: ${subscriberCount}`);
+    console.log(`Jumlah Video: ${videoCount}`);
+    console.log(`Deskripsi Channel: ${channelDescription}`);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+// Panggil fungsi untuk cek informasi channel
+getChannelInfo();
